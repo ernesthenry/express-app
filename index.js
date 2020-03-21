f7b9f36f0d370621871d42d8c88de963b6cfe834
ernesthenry <henry38ernest@gmail.com>
2020-03-22 00:38:30 +0300

    :

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req,res)=>
+res.send(` a get request with / route on port ${PORT}`)
+)
+app.post("/newitem", (req,res)=>
+res.send(`a post request with /newitem route on port ${PORT}`)
+)
+app.put("/item", (req,res)=>
+res.send(`a put request with /item route on port ${PORT}`)
+)
+app.post("/newitem", (req,res)=>
+res.send(`a delete request with /item route on port ${PORT}`)
+)
+
 app.listen(PORT, ()=> {
     console.log(`Your server is running on port ${PORT}`)
     console.log(data)
