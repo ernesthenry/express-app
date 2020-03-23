2c5b639c6f76ce63772febb879620779a4b2101b
ernesthenry <henry38ernest@gmail.com>
2020-03-23 22:55:41 +0300

    Add parameters to the code for getting a specific item

--- index.js
+++ index.js
@@ -15,18 +15,22 @@ app.use('/images', express.static('images'))
 app.get("/", (req,res)=>
 res.json(data)
 )
+
+app.get('/item/:id', (req,res) =>{
+    console.log(req.params.id);
+    let user = Number(req.params.id);
+    console.log(user);
+    console.log(data[user])
+    res.send(data[user]);
+})
+
 app.post("/newItem", (req,res)=>
 res.send(`a post request with /newItem route on port ${PORT}`)
 )
 app.put("/item", (req,res)=>
 res.send(`a put request with /item route on port ${PORT}`)
 )
-app.post("/newitem", (req,res)=>
-res.send(`a delete request with /item route on port ${PORT}`)
-)
-
 app.listen(PORT, ()=> {
     console.log(`Your server is running on port ${PORT}`)
-    console.log(data)
 });
 
