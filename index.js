ba0d587236e42bb46746cd347999db93b310cec8
ernesthenry <henry38ernest@gmail.com>
2020-03-24 12:53:59 +0300

    add basic middleware

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { request } from "express"
 import data from './data/data'
 
 const app = express();
@@ -17,32 +17,39 @@ app.use('/images', express.static('images'))
 // )
 
 app.get('/item/:id', (req,res, next) =>{
+    // This is the middleware that pulls the data
     console.log(req.params.id);
     let user = Number(req.params.id);
     console.log(user);
     console.log(data[user])
+    // middleware that uses the request object
+    console.log(`Request from: ${req.originalUrl}`)
+    console.log(`Request type: ${req.method}`)
+
+    // everything above is middleware
     res.send(data[user]);
     next()
 }, (req,res) =>
-console.log('Did you get the right data?')
+    console.log('Did you get the right data?')
 );
 
 app.route('/item')
 .get((req, res) => {
-// res.download("images/ernest.jpg")
-// res.redirect('http://www.linkedin.com')
-// res.end()  Ends a call to an api
-res.send(`a get request with /item route on port ${PORT}`)
+    // res.download("images/ernest.jpg")
+    // res.redirect('http://www.linkedin.com')
+    // res.end()  Ends a call to an api
+    res.send(`a get request with /item route on port ${PORT}`)
 })
 .post((req,res)=>
-res.send(`a post request with /newItem route on port ${PORT}`)
+    res.send(`a post request with /newItem route on port ${PORT}`)
 )
 .put((req,res)=>
-res.send(`a put request with /item route on port ${PORT}`)
+    res.send(`a put request with /item route on port ${PORT}`)
 )
 .delete((req,res)=>
-res.send(`a delete request with /item route on port ${PORT}`)
+    res.send(`a delete request with /item route on port ${PORT}`)
 )
+
 app.listen(PORT, ()=> {
     console.log(`Your server is running on port ${PORT}`)
 });
